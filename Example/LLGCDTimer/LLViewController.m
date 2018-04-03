956b6666a06ac974f1e2a2d48076040feccfce81
liuli <lypcliuli@163.com>
2018-04-03 10:16:55 +0800

    添加注释

--- Example/LLGCDTimer/LLViewController.m
+++ Example/LLGCDTimer/LLViewController.m
@@ -16,6 +16,7 @@
 @implementation LLViewController
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // 使用方法
     [LLGCDTimer llGCDTimer:10 repeat:YES interval:5 progress:^(NSInteger current) {
         NSLog(@"%zd", current);
     } endAction:^(NSInteger current) {
